80c16f1f825fe3da64979b770fa11296c4a61007
Nahuel Enriquez <61632891+Trainxp@users.noreply.github.com>
2022-07-06 18:03:49 -0300

    Desafio landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Navbar from './componentes/Navbar/Navbar';
+import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
 
 function App() {
+    
   return (
     <div className="App">
         <Navbar/>
+        <ItemListContainer greeting= "Bienvenido a Nexus" />
+
     </div>
   );
 }
